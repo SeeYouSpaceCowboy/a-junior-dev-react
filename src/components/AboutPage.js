b5b515bcb1387589dce2c0caba180bc2c5bbbef5
Mohammed Chisti <Chisti@Mohammeds-MacBook-Air.local>
2017-04-04 22:22:36 -0400

    Start with Buildpack heroku

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -3,13 +3,16 @@ import React, { Component } from  'react'
 class AboutPage extends Component {
   render() {
     return (
-      <div className="flexbox-center">
-        <div className='card'>
-          <h1>About</h1>
-          <p>
-            This site was created for people who are having a difficult time
-            getting their foot in the door.
-          </p>
+      <div>
+        <div className='space'></div>
+        <div className='flexbox-center'>
+          <div className='card'>
+            <h1>About</h1>
+            <p>
+              This site was created for people who are having a difficult time
+              getting their foot in the door.
+            </p>
+          </div>
         </div>
       </div>
     )
