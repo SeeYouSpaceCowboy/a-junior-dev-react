32ce4bf56bdb6c9ed1705ec15bb0036baef437c3
Mohammed Chisti <Chisti@Mohammeds-MacBook-Air.local>
2017-04-30 14:14:38 -0400

    Changes to production

--- src/adapters/userAdapter.js
+++ src/adapters/userAdapter.js
@@ -1,10 +1,10 @@
 import { browserHistory } from 'react-router'
 import axios from 'axios'
 
-// const url = 'https://a-junior-dev.herokuapp.com/v1/'
-const dev_url = 'http://localhost:3000/v1/'
+const url = 'https://a-junior-dev.herokuapp.com/v1/'
+// const dev_url = 'https://localhost:3000/v1/'
 
-axios.defaults.baseURL = dev_url
+axios.defaults.baseURL = url
 axios.defaults.headers.common['AUTHORIZATION'] = sessionStorage.getItem('jwt')
 
 export const userAdapter = {
